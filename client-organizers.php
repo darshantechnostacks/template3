57982337aeb1004f742d90b5f069f1f74b05c675
techno <prachi@technostacks.com>
2018-11-15 16:52:11 +0530

    marge code

---
+++ client-organizers.php
@@ -0,0 +1,73 @@
+<?php 
+    require_once('header.php'); 
+    
+    $request = array(
+    'conditions' => ['is_deleted' => 0,'status'=>1,'is_setting'=>1],
+    'fields' => array(),
+    'select' => array(),
+    'get' => 'all',
+    'order' => array('id' => 'desc')
+
+    );
+    $client_forms_setting = $curl->send_api($request, 'ClientOrganizer/index');
+    
+    if($client_forms_setting->code == 200)
+    {
+        $client_formssetting = $client_forms_setting->ClientOrganizer;
+        if(count($client_formssetting) > 0)
+        {
+            $images = isset($client_formssetting[0]->document) ? $client_formssetting[0]->document  :'';
+            $description = isset($client_formssetting[0]->description) ? $client_formssetting[0]->description  :'';
+        }
+    }
+
+
+    $request = array(
+    'conditions' => ['is_deleted' => 0,'status'=>1,'type'=>1,'is_setting'=>0],
+    'fields' => array(),
+    'select' => array(),
+    'get' => 'list',
+    'order' => array('id' => 'desc')
+
+    );
+
+    $client_forms = $curl->send_api($request, 'ClientOrganizer/index');
+   
+    if($client_forms->code == 200)
+    {
+        $clientforms = $client_forms->ClientOrganizer;
+    }
+?>
+            <div class="page_top_wrap page_top_title page_top_breadcrumbs" style="background:url(<?php echo API_URL ?>geturl/uploads/document/<?php echo $images; ?>) no-repeat 59% 49%;max-height: 250px;background-size: cover;">
+                <div class="content_wrap">
+                    <div class="breadcrumbs">
+                        <a class="breadcrumbs_item home" href="index.php">Home</a>
+                        <span class="breadcrumbs_delimiter"></span>
+                        <span class="breadcrumbs_item current">Client Organizers</span>
+                    </div>
+                    <h1 class="page_title">Client Organizers</h1>
+                </div>
+            </div>
+            <div class="container padding_bottom_small padding_top_small">
+            <?php echo $description; ?>
+                <h2 class="sc_title sc_align_center aligncenter fig_border">Download the latest tax forms below</h2>
+                <div class="row d-flex flex-wrap">
+                <?php  if(count($clientforms) > 0){ foreach($clientforms as $client_form){?>
+                    <div class="col-sm-6 col-xs-12 margin_bottom_small">
+                        <a target="blank" href="<?php echo API_URL ?>geturl/uploads/document/<?php echo $client_form->document; ?>" class="d-flex file-list">
+                            <div class="d-flex align-items-center file-icon padding_bottom_mini padding_left_mini padding_right_mini padding_top_mini">
+                                <i class="fa fa-3x fa-file-pdf-o"></i>
+                            </div>
+                            <div class="file-content d-flex align-items-center flex-grow-1 padding_bottom_mini padding_left_mini padding_right_mini padding_top_mini">
+                                <h5 class="margin_bottom_none font-size-16 file-title"><?php echo $client_form->title;?></h5>
+                            </div>
+                        </a>
+                    </div>
+                    <?php } }else{?>
+                        <div class="col-sm-12 col-xs-12 margin_bottom_small">
+                            Record not found
+                        </div>
+                    <?php } ?>
+                </div>
+            </div>
+<?php require_once('footer.php'); ?>
---
+++ record-retention.php
@@ -0,0 +1,77 @@
+<?php 
+    require_once('header.php'); 
+    // $about_data = array();
+    $data['websiteId'] = WEBSITE_ID;
+
+    $request = array(
+    'conditions' => ['is_deleted' => 0,'status'=>1,'is_setting'=>0],
+    'fields' => array(),
+    'select' => array(),
+    'get' => 'all',
+    'order' => array('id' => 'desc')
+
+    );
+    $RecordRetention = $curl->send_api($request, 'RecordRetention/index');
+    if($RecordRetention->code == 200)
+    {
+        $recordretention = $RecordRetention->RecordRetention;
+    }
+
+
+    $request = array(
+    'conditions' => ['is_deleted' => 0,'status'=>1,'is_setting'=>1],
+    'fields' => array(),
+    'select' => array(),
+    'get' => 'list',
+    'order' => array('id' => 'desc')
+
+    );
+    $RSetting = $curl->send_api($request, 'RecordRetention/index');
+    $images = $description = '';
+    if($RSetting->code == 200)
+    {
+        $RSetting = $RSetting->RecordRetention;
+        if(count($RSetting) > 0)
+        {
+            $images = isset($RSetting[0]->photo) ? $RSetting[0]->photo  :'';
+            $description = isset($RSetting[0]->description) ? $RSetting[0]->description  :'';
+        }
+    }
+    
+    /*if (!empty($Uretention) && $Uretention->code == 200) {
+        $taxcenter_data = $Uretention->UtaxCenter;
+
+        $images = isset($taxcenter_data->image) ? $Uretention->image  :'';
+        $description = isset($taxcenter_data->description) ? $Uretention->description  :'';
+        $is_edit = isset($taxcenter_data->is_edit) ? $taxcenter_data->is_edit  :'';
+        
+        if($is_edit == 0)
+        {
+            $images = isset($taxcenter_data->TaxCenter->image) ? $taxcenter_data->TaxCenter->image  :'';
+            $description = isset($taxcenter_data->TaxCenter->description) ? $taxcenter_data->TaxCenter->description  :'';
+        }
+    }*/
+?>
+    <div class="page_top_wrap page_top_title page_top_breadcrumbs" style="background:url(<?php echo API_URL ?>geturl/uploads/photo/<?php echo $images; ?>) no-repeat 59% 49%;max-height: 250px;background-size: cover;">
+        <div class="content_wrap">
+            <div class="breadcrumbs">
+                <a class="breadcrumbs_item home" href="#!">Home</a>
+                <span class="breadcrumbs_delimiter"></span>
+                <a class="breadcrumbs_item home" href="resources.html">Resources</a>
+                <span class="breadcrumbs_delimiter"></span>
+                <a class="breadcrumbs_item home" href="tax-center.html">Tax Center</a>
+                <span class="breadcrumbs_delimiter"></span>
+                <span class="breadcrumbs_item current">Record Retention</span>
+            </div>
+            <h1 class="page_title">Record Retention</h1>
+        </div>
+    </div>
+    <center><h4 class="page_title"><?php echo $description;?></h4></center>
+        <div class="page_content_wrap margin_bottom_mini padding_bottom_mini padding_left_mini padding_right_mini padding_top_mini">
+            <?php 
+            foreach($recordretention  as $row){?>
+                <p class="margin_bottom_mini"><strong><?php echo $row->title;?></strong></p>
+                <p class="margin_bottom_mini margin_top_none"><?php echo $row->description;?> </p>
+           <?php }?>
+        </div>
+<?php require_once('footer.php'); ?> 
