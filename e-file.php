21698009e66794332e2414d359be5b29d2076609
techno <prachi@technostacks.com>
2018-11-21 15:05:54 +0530

    e-form page

---
+++ e-file.php
@@ -0,0 +1,130 @@
+<?php require_once('header.php') ?>
+    <div class="sub-page-banner mb3">
+        <div class="banner-img">
+            <img src="img/efile_header.jpeg" class="img-responsive">
+        </div>
+        <div class="page-title">
+            <div class="container">
+                <div class="row">
+                    <div class="col-md-12">
+                        <h2>E-file Authorization</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="container mb3">
+        <h2 class="font-medium">Employer forms</h2>
+        <div class="row">
+            <div class="col-md-4 col-xs-12">
+                <a href="#." class="d-flex file-list mb1">
+                    <div class="align-items-center d-flex file-icon">
+                        <i class="fa fa-3x fa-file-pdf-o"></i>
+                    </div>
+                    <div class="align-items-center d-flex file-content flex-grow-1">
+                        <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension Form
+                            4868</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-xs-12">
+                <a href="#." class="d-flex file-list mb1">
+                    <div class="align-items-center d-flex file-icon">
+                        <i class="fa fa-3x fa-file-pdf-o"></i>
+                    </div>
+                    <div class="align-items-center d-flex file-content flex-grow-1">
+                        <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension Form
+                            4868</h5>
+                    </div>
+                </a>
+            </div>
+            <div class="col-md-4 col-xs-12">
+                <a href="#." class="d-flex file-list mb1">
+                    <div class="align-items-center d-flex file-icon">
+                        <i class="fa fa-3x fa-file-pdf-o"></i>
+                    </div>
+                    <div class="align-items-center d-flex file-content flex-grow-1">
+                        <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension Form
+                            4868</h5>
+                    </div>
+                </a>
+            </div>
+        </div>
+    </div>
+    <div class="bg-warning">
+        <div class="container mb3">
+
+
+            <h2 class="font-medium">Individual forms</h2>
+            <div class="row">
+
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-4 col-xs-12">
+                    <a href="#." class="d-flex file-list mb1">
+                        <div class="align-items-center d-flex file-icon">
+                            <i class="fa fa-3x fa-file-pdf-o"></i>
+                        </div>
+                        <div class="align-items-center d-flex file-content flex-grow-1">
+                            <h5 class="margin_bottom_none font-medium font-size-16 file-title">2017 Federal Extension
+                                Form 4868</h5>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+<?php require_once('footer.php') ?>
